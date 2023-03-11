5630fe3f15c70e86d8b586d0af22724c3810f58f
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-03-11 16:11:53 -0300

    fix: .env

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { SetupServer } from "./server";
-
+import { config } from "dotenv";
+config();
 (async (): Promise<void> => {
   const server = new SetupServer(3333);
   server.init();
--- src/messages/candle-messages-channel.ts
+++ src/messages/candle-messages-channel.ts
@@ -6,7 +6,6 @@ import { ICandleRepository } from "@src/repositories/candle.repository";
 
 export class CandleMessageChannel {
   private channel: Channel;
-  private QUEUE: string = "candles";
   private _io: Server;
 
   constructor(server: http.Server, private readonly repo: ICandleRepository) {
@@ -22,18 +21,22 @@ export class CandleMessageChannel {
 
   private async _createMessageChannel() {
     try {
-      const connection = await connect("amqp://dev:senhadev@127.0.0.1:5672");
+      const connection = await connect(
+        process.env.AMQP_SERVER ?? "amqp://dev:senhadev@127.0.0.1:5672"
+      );
       this.channel = await connection.createChannel();
-      this.channel.assertQueue(this.QUEUE);
+      this.channel.assertQueue(process.env.QUEUE_NAME ?? "candles");
     } catch (err: any) {
       console.log(err.message);
     }
   }
   async messageConsumer() {
     await this._createMessageChannel();
-    this.channel.consume(this.QUEUE, async (msg) => {
+    this.channel.consume(process.env.QUEUE_NAME ?? "candles", async (msg) => {
       if (msg) {
         const candle: CandleModel = JSON.parse(msg.content.toString());
+        console.log("-----------------------------------");
+        console.log("Message received successfully", candle);
         this.channel.ack(msg);
         await this.repo.save(candle);
         this._io.emit("newCandle", candle);
